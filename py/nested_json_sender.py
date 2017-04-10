a93d4c3ee21cf66d082d9c5ae2f44c2f91b0948e
Albert Lam <alam-r7@users.noreply.github.com>
2017-04-10 10:19:12 -0400

    Update nested_json_sender.py

--- py/nested_json_sender.py
+++ py/nested_json_sender.py
@@ -6,14 +6,12 @@ import time
 token = 'TOKEN'
 HOST = 'data.logentries.com'
 PORT = 10000
-tts = 36318083.0
-secs = 86400.0
 
 lines = str({"song": [{"title": "Never gonna drop your logs","contributors": {"artist": "Big","producer": "Team","songwriter": "Win!"},"plays": "4889000","publicationDate": "Tue Jun 17 20:31:33 CEST 2014","popularity": 8.9615083,"timestamp": "1403029893606","genre": "Melodic Death-Metal"}],"website": [{"Url": "http://www.notlikethis.org/giphy/contentblob/368/dab.gif","micropost": {"plainText": "Dab on 'em"},"hits": 4365,"timestamp": 1432410342400,"type": "image/gif"}]})
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-for i in range(0, int(tts)):
+while True:
 	s.sendall('%s %s\n' % (token, lines))
-	time.sleep(secs/tts)
+	time.sleep(.00237)
 s.close
