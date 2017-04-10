1c0f8f7982ea9f6e0fca46f96dfd1a1ccd3e0621
Albert Lam <alam-r7@users.noreply.github.com>
2017-04-10 10:18:22 -0400

    Update access_sender.py

--- py/access_sender.py
+++ py/access_sender.py
@@ -3,14 +3,12 @@ import socket
 
 token = 'TOKEN'
 line = '{ "time": "[2017-03-06 12:18:39 +0000]", "remoteIP": "127.18.244.47", "host": "Server 1", "request": "/docs", "query": "", "method": "GET", "status": "200", "duration": "554", "bytes": "383", "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36", "referer": "-" }'
-tts = 48459302.0
-secs = 86400.0
 
 HOST = 'data.logentries.com'
 PORT = 10000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-for i in range(0, int(tts)):
+while True:
 	s.sendall('%s %s\n' % (token, line))
-	time.sleep(secs/tts)
+	time.sleep(.0017)
 s.close
