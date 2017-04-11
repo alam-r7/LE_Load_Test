a69d1f66e022eb91ff66de40a75dd3351fa22964
Albert Lam <alam-r7@users.noreply.github.com>
2017-04-11 11:39:53 -0400

    Update raw_launch.sh

--- py/raw_launch.sh
+++ py/raw_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if (ps aux | pgrep -f "/usr/bin/python /home/ec2-user/py/raw_log_sender.py" > /dev/null)
+if (ps aux | pgrep -f "/usr/bin/python /home/ec2-user/LE_Load_Test/py/raw_log_sender.py" > /dev/null)
 then
     echo "no scripts started"
 else
-    /usr/bin/python /home/ec2-user/py/raw_log_sender.py
+    /usr/bin/python /home/ec2-user/LE_Load_Test/py/raw_log_sender.py
 fi
