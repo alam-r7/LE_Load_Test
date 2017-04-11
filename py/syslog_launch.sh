c6d0f812cc4b08069265b8329dc65198c5bb46f8
Albert Lam <alam-r7@users.noreply.github.com>
2017-04-11 11:40:12 -0400

    Update syslog_launch.sh

--- py/syslog_launch.sh
+++ py/syslog_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if (ps aux | pgrep -f "/usr/bin/python /home/ec2-user/py/syslog_sender.py" > /dev/null)
+if (ps aux | pgrep -f "/usr/bin/python /home/ec2-user/LE_Load_Test/py/syslog_sender.py" > /dev/null)
 then
         echo "no scripts started"
 else
-        /usr/bin/python /home/ec2-user/py/syslog_sender.py
+        /usr/bin/python /home/ec2-user/LE_Load_Test/py/syslog_sender.py
 fi
